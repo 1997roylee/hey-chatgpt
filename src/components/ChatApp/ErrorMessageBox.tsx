c5ec29887500bec7bd4766db34d7632c600d02f4
Roy <53046899+1997roylee@users.noreply.github.com>
2022-12-12 00:53:13 +0800

    design a error message box to display error

---
+++ src/components/ChatApp/ErrorMessageBox.tsx
@@ -0,0 +1,54 @@
+import { Box, Button, Flex, Text } from '@mantine/core'
+import { AppState, useAppStore } from '../../stores'
+import { OpenAi } from '../OpenAi'
+
+export const ErrorMessageBox = (): JSX.Element => {
+    const { setIsReverseProxyMode, isReverseProxyMode } = useAppStore((state: AppState) => ({
+        setIsReverseProxyMode: state.setIsReverseProxyMode,
+        isReverseProxyMode: state.isReverseProxyMode,
+    }))
+
+    const handleClick = (): void => {
+        setIsReverseProxyMode(!isReverseProxyMode)
+    }
+
+    return (
+        <Box bg={'#fff'} p={12} w='auto'>
+            <Flex w='100%'>
+                <Box
+                    sx={{
+                        width: 24,
+                    }}
+                    mr={12}
+                >
+                    <OpenAi w={24} />
+                </Box>
+                <Box
+                    sx={{
+                        flex: 1,
+                    }}
+                >
+                    <Text align='left'>
+                        Before you use this extension, you need to log in to OpenAI&apos;s website.{' '}
+                        <Text
+                            component='a'
+                            target='_blank'
+                            href={'https://chat.openai.com/auth/login'}
+                        >
+                            https://chat.openai.com/auth/login
+                        </Text>
+                    </Text>
+                    <Text mb={'8px'}>
+                        Or you can enable reverse proxy mode to use this extension without logging
+                        in to OpenAI&apos;s website.
+                    </Text>
+                    <Button onClick={handleClick} color={isReverseProxyMode ? 'green' : 'blue'}>
+                        {isReverseProxyMode
+                            ? 'Disable reverse proxy mode'
+                            : 'Enable reverse proxy mode'}
+                    </Button>
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
