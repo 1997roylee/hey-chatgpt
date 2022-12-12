34cac0349ea8b3f66cfb3a3de7c6d3bca581aacf
Roy <53046899+1997roylee@users.noreply.github.com>
2022-12-12 13:16:55 +0800

    display Tutorial

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Chatbot } from './app'
+import { Tutorial } from './app'
 import './index.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Chatbot />
+        <Tutorial />
     </React.StrictMode>,
 )
