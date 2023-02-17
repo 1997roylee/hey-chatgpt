ece3b52ceca929865e7848ebc95b0c8cd8902cab
Roy <53046899+1997roylee@users.noreply.github.com>
2023-02-17 13:50:33 +0800

    add submit button

--- src/components/ChatApp/MessageInput.tsx
+++ src/components/ChatApp/MessageInput.tsx
@@ -1,6 +1,7 @@
-import { Box, Input } from '@chakra-ui/react'
+import { Input, Icon, Flex, IconButton } from '@chakra-ui/react'
 import { useCallback, useState } from 'react'
 import Browser from 'webextension-polyfill'
+import { MdSend } from 'react-icons/md'
 import { AppState, useAppStore } from '../../stores'
 
 export const MessageInput = (): JSX.Element => {
@@ -17,46 +18,49 @@ export const MessageInput = (): JSX.Element => {
         setValue(event.target.value)
     }, [])
 
+    const handleSubmit = (): void => {
+        if (value.length === 0) return
+
+        addMessage({
+            id: Date.now().toString(),
+            text: value,
+            sender: 'me',
+            index: 0,
+        })
+        setIsLoading(true)
+        setValue('')
+        void Browser.runtime.sendMessage({
+            type: 'chat',
+            payload: value,
+            parentMessageId: lastConversationId,
+            proxy: isReverseProxyMode,
+        })
+    }
+
     const handleKeyDown = (event: any): void => {
         if (event.key === 'Enter') {
-            if (value.length === 0) return
-
-            addMessage({
-                id: Date.now().toString(),
-                text: value,
-                sender: 'me',
-                index: 0,
-            })
-            setIsLoading(true)
-            setValue('')
-            void Browser.runtime.sendMessage({
-                type: 'chat',
-                payload: value,
-                parentMessageId: lastConversationId,
-                proxy: isReverseProxyMode,
-            })
+            handleSubmit()
         }
     }
 
     return (
-        <Box
-            sx={{
-                position: 'absolute',
-                top: -24,
-                left: 8,
-                right: 8,
-                '--shadow-1': 'rgba(0, 0, 0, 0.1)',
-                '--shadow-2': 'rgba(0, 0, 0, 0.2)',
-                boxShadow: '0 2px 4px var(--shadow-1),0 12px 28px var(--shadow-2)',
-            }}
-        >
+        <Flex px={4} py={2} alignItems='center'>
             <Input
                 placeholder='Your message'
                 value={value}
+                // size='lg'
+                flex={1}
+                border='1px solid #eee'
                 onChange={handleChange}
                 onKeyDown={handleKeyDown}
                 autoFocus
             />
-        </Box>
+            <IconButton
+                onClick={handleSubmit}
+                ml={1}
+                aria-label='Send'
+                icon={<Icon as={MdSend} color='#000' size={8} />}
+            />
+        </Flex>
     )
 }
