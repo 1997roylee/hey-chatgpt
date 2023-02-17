ad52fc581ee5d99faa7a398d7e16b7f46bcf916a
Roy <53046899+1997roylee@users.noreply.github.com>
2023-02-17 13:50:39 +0800

    update error message

--- src/components/ChatApp/ErrorMessageBox.tsx
+++ src/components/ChatApp/ErrorMessageBox.tsx
@@ -1,51 +1,41 @@
 import { Box, Button, Flex, Text } from '@chakra-ui/react'
-import { AppState, useAppStore } from '../../stores'
-import { OpenAi } from '../OpenAI'
+// import { AppState, useAppStore } from '../../stores'
+import { OpenAI } from '../OpenAI'
 
 export const ErrorMessageBox = (): JSX.Element => {
-    const { setIsReverseProxyMode, isReverseProxyMode } = useAppStore((state: AppState) => ({
-        setIsReverseProxyMode: state.setIsReverseProxyMode,
-        isReverseProxyMode: state.isReverseProxyMode,
-    }))
+    // const { setIsReverseProxyMode, isReverseProxyMode } = useAppStore((state: AppState) => ({
+    //     setIsReverseProxyMode: state.setIsReverseProxyMode,
+    //     isReverseProxyMode: state.isReverseProxyMode,
+    // }))
 
-    const handleClick = (): void => {
-        setIsReverseProxyMode(!isReverseProxyMode)
-    }
+    // const handleClick = (): void => {
+    //     setIsReverseProxyMode(!isReverseProxyMode)
+    // }
 
     return (
-        <Box bg={'#fff'} p={12} w='auto'>
+        <Box bg={'#fff'} p={3} w='auto'>
             <Flex w='100%'>
-                <Box
-                    sx={{
-                        width: 24,
-                    }}
-                    mr={12}
-                >
-                    <OpenAi w={24} />
+                <Box width='28px' mr={1}>
+                    <OpenAI w={6} />
                 </Box>
                 <Box
                     sx={{
                         flex: 1,
                     }}
                 >
-                    <Text align='left'>
-                        Before you use this extension, you need to log in to OpenAI&apos;s website.{' '}
-                        <Text
-                            as='a'
-                            target='_blank'
-                            href={'https://chat.openai.com/auth/login'}
-                        >
-                            https://chat.openai.com/auth/login
+                    <Text align='left' mb={1}>
+                        {/* Before you use this extension, you need to log in to OpenAI&apos;s website.{' '} */}
+                        <Text as='a' target='_blank' href={'https://chat.openai.com/auth/login'}>
+                            Please login and pass Cloudflare check at chat.openai.com
                         </Text>
                     </Text>
-                    <Text mb={'8px'}>
-                        Or you can enable reverse proxy mode to use this extension without logging
-                        in to OpenAI&apos;s website.
-                    </Text>
-                    <Button onClick={handleClick} color={isReverseProxyMode ? 'green' : 'blue'}>
-                        {isReverseProxyMode
-                            ? 'Disable reverse proxy mode'
-                            : 'Enable reverse proxy mode'}
+                    <Button
+                        fontWeight={400}
+                        as='a'
+                        target='_blank'
+                        href={'https://chat.openai.com/auth/login'}
+                    >
+                        Login to OpenAI
                     </Button>
                 </Box>
             </Flex>
