1bca637391cdb1d568430311af84254bb1cba388
Roy <53046899+1997roylee@users.noreply.github.com>
2023-02-17 13:49:57 +0800

    add animation

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@mantine/core'
-import { OpenAi } from './OpenAi'
+import { Box } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
+import { OpenAI } from './OpenAI'
 
 interface Props {
     onClick?: () => void
@@ -9,28 +10,33 @@ export const Cursor = ({ onClick }: Props): JSX.Element => {
     return (
         <Box
             sx={{
-                // zIndex: 9999,
+                zIndex: 9999,
                 position: 'relative',
             }}
-            right={24}
-            bottom={24}
+            right={6}
+            bottom={6}
             onClick={onClick}
         >
             <Box
                 sx={{
-                    width: 64 - 16,
-                    height: 64 - 16,
-                    borderRadius: 64 / 2,
+                    width: 12,
+                    height: 12,
+                    borderRadius: '50%',
                     backgroundColor: '#fff',
                     '--shadow-1': 'rgba(0, 0, 0, 0.1)',
                     '--shadow-2': 'rgba(0, 0, 0, 0.2)',
                     boxShadow: '0 2px 4px var(--shadow-1),0 12px 28px var(--shadow-2)',
                     cursor: 'pointer',
-                    padding: 8,
+                    padding: 2,
                 }}
             >
-                <OpenAi />
-                {/* <Image src={Openai} w='100%' h='100%' /> */}
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.21 }}
+                >
+                    <OpenAI />
+                </motion.div>
             </Box>
         </Box>
     )
